03bb4a926a4f12fb69474498bc64bb290e5035ae
Naren <deadlyassassin2003@gmail.com>
2025-03-02 08:55:50 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scan all JS/TSX files for Tailwind classes
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
